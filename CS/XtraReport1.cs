71eaed194c6cdd87554ba12059a88687bf3a98b9
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-26 00:04:53 +0400

    Change example status to out of maintenance

--- CS/XtraReport1.cs
+++
@@ -1,9 +0,0 @@
-
-namespace FormatDecimals {
-    public partial class myReport : DevExpress.XtraReports.UI.XtraReport {
-        public myReport() {
-            InitializeComponent();
-        }
-
-    }
-}
